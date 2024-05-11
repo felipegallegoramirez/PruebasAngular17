34d226a20d5c1d37f7dad72c9e4654ca40075a9e
Felipe Gallego <felipegallego600@gmail.com>
2024-05-10 21:58:23 -0500

    alo

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: "https://polinet.vercel.app/",
+    baseUrl: "https://polinet.vercel.app/#/",
     backend:"https://polinet.onrender.com/"
   };
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: "https://polinet.vercel.app/",
+    baseUrl: "https://polinet.vercel.app/#/",
     backend:"https://polinet.onrender.com/"
   };
